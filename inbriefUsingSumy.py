81a4cabc23d01a2b9da246e9f70dc8f646cdca26
jeff007ali <jagharia@gmail.com>
2020-02-25 00:05:52 +0530

    Used Text Rank Summarizer instead of LSA Summarizer

--- inbriefUsingSumy.py
+++ inbriefUsingSumy.py
@@ -5,6 +5,7 @@ from sumy.parsers.html import HtmlParser
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lsa import LsaSummarizer as Summarizer
+from sumy.summarizers.text_rank import TextRankSummarizer
 from sumy.nlp.stemmers import Stemmer
 from sumy.utils import get_stop_words
 
@@ -29,14 +30,15 @@ So become intentional on what you want out of life. Commit to it. Nurture your d
 
 
 if __name__ == "__main__":
-    url = "https://en.wikipedia.org/wiki/Automatic_summarization"
-    # parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
+    url = "https://becominghuman.ai/text-summarization-in-5-steps-using-nltk-65b21e352b65"
+    parser = HtmlParser.from_url(url, Tokenizer(LANGUAGE))
     # or for plain text files
     # parser = PlaintextParser.from_file("document.txt", Tokenizer(LANGUAGE))
-    parser = PlaintextParser.from_string(text_str, Tokenizer(LANGUAGE))
+    # parser = PlaintextParser.from_string(text_str, Tokenizer(LANGUAGE))
     stemmer = Stemmer(LANGUAGE)
 
-    summarizer = Summarizer(stemmer)
+    # summarizer = Summarizer(stemmer)
+    summarizer = TextRankSummarizer(stemmer)
     summarizer.stop_words = get_stop_words(LANGUAGE)
 
     for sentence in summarizer(parser.document, SENTENCES_COUNT):
